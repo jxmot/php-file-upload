d56ad384662af9ba20fcd4c8b41062ca8ca227bb
jxmot <webdev@stupiddog.net>
2017-09-14 23:02:04 -0500

    comments & minor changes.

--- upload.php
+++ upload.php
@@ -7,41 +7,58 @@
 </head>
 <body>
 <?php
-/* will be copied in to the event data payload a the end of this file. */
+// will be copied in to the event data payload a the end of this file
 $error = 0;
 $errmsg = "success";
 
-$filename = "";
-$filetype = "";
-$filesize = "";
-$filepath = "";
+// placed here due to needed scope
+$filename  = "";
+$filetype  = "";
+$filesize  = "";
+$filepath  = "";
+$tmpfile   = "";
 $ufiletype = "";
 
 // Check if the form was submitted
 if($_SERVER["REQUEST_METHOD"] == "POST") {
+
     // Check if file was uploaded without errors
     if(isset($_FILES["uploadfile"]) && $_FILES["uploadfile"]["error"] == 0) {
 
         $allowed = array("htm" => "text/html", "html" => "text/html", "md" => "text/html", "txt" => "text/plain");
 
         $filename = $_FILES["uploadfile"]["name"];
-        $filetype = $_FILES["uploadfile"]["type"];
+
+        // NOTE: This is the browser supplied file type, which 
+        // is totally wrong in most cases. Don't use it. It will 
+        // be a LIE! Instead check the file type of the file
+        // after it's been uploaded to the temporary folder.
+        //
+        // see : https://stackoverflow.com/questions/1201945/how-is-mime-type-of-an-uploaded-file-determined-by-browser
+        // 
+        // BAD :
+        // $filetype = $_FILES["uploadfile"]["type"];
+        // GOOD :
+        $tmpfile  = $_FILES["uploadfile"]["tmp_name"];
+        // UGLY: The exact location is determined by settings 
+        // in your HTTP server.
+    
+        // This is the real file type...
+        $filetype = mime_content_type($tmpfile);
+
         $filesize = $_FILES["uploadfile"]["size"];
         $filepath = $_POST["path"];
 
         echo "File Name : " . $filename . "<br>";
         echo "File Type : " . $filetype . "<br>";
         echo "File Size : " . (($filesize / 1024) | 0) . " KB<br>";
-        echo "Uploaded to : " . $_FILES["uploadfile"]["tmp_name"] . "<br><br><br>";
+
+        echo "Uploaded to : {$tmpfile}<br><br><br>";
 
         // Verify file extension
         $ext = pathinfo($filename, PATHINFO_EXTENSION);
 
-        // NOTE: jmotyl - the call to array_key_exists() here is NOT enough.
-        // That's because if a file comes across as a known type, but isn't 
-        // associated by extension that file will upload. The proper fix
-        // would be to change the parent so that correct type is sent.
-
+        // is the extension allowed?
         if(!array_key_exists($ext, $allowed)) {
             $errmsg = "Please select a valid file format, {$ext} is not allowed";
             $error  = -5;
