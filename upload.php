b636b0f63c36ebe038187bf0469e2399570fce4c
jxmot <webdev@stupiddog.net>
2017-09-14 21:49:27 -0500

    wip: refactoring code, added error message & code handling

--- upload.php
+++ upload.php
@@ -7,11 +7,17 @@
 </head>
 <body>
 <?php
+/* will be copied in to the event data payload a the end of this file. */
 $error = 0;
-$errmsg = "";
+$errmsg = "success";
+
+$filename = "";
+$filetype = "";
+$filesize = "";
+$ufiletype = "";
 
 // Check if the form was submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+if($_SERVER["REQUEST_METHOD"] == "POST") {
     // Check if file was uploaded without errors
     if(isset($_FILES["uploadfile"]) && $_FILES["uploadfile"]["error"] == 0) {
         $allowed = array("htm" => "text/html", "html" => "text/html", "md" => "text/html", "txt" => "text/plain", "log" => "application/octet-stream");
@@ -32,42 +38,59 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         // associated by extension that file will upload. The proper fix
         // would be to change the parent so that correct type is sent.
 
-        if(!array_key_exists($ext, $allowed)) die("Error: Please select a valid file format.");
-    
-        // Verify file size - 100k maximum
-        $maxsize = 100 * 1024;
-        if($filesize > $maxsize) die("Error: File size is larger than the allowed limit.");
-    
-        // Verify MYME type of the file
-        if(in_array($filetype, $allowed)){
-            // Check whether file exists before uploading it
-            //if(file_exists("upload/" . $_FILES["uploadfile"]["name"])){
-            if(file_exists("upload/" . $filename)){
-                echo $filename . " is already present.";
-            } else{
-                move_uploaded_file($_FILES["uploadfile"]["tmp_name"], "upload/" . $filename);
-                echo "Your file was uploaded & moved successfully.<br>";
-                $ufiletype = mime_content_type("upload/" . $filename);
-                echo "uploaded file type : {$ufiletype}<br>";
-            } 
-        } else{
-            echo "Error: There was a problem uploading your file. Please try again.<br><br>";
-            $ret = in_array($filetype, $allowed);
-            echo "in_array : " . $ret . "<br><br>";
-            echo "filetype : " . $filetype . "<br><br>";
-            // only prints half, just values and no keys
-            //$_allowed='("'.implode('", "', $allowed).'")';
-            // try? - json_encode()
-            //echo "allowed  : " . $_allowed . "<br><br>";
+        if(!array_key_exists($ext, $allowed)) {
+            //die("Error: Please select a valid file format.");
+            $errmsg = "Please select a valid file format, {$ext} is not allowed";
+            $error  = -5;
+        } else {
+            // Verify file size - 100k maximum
+            $maxsize = 100 * 1024;
+            if($filesize > $maxsize) {
+                //die("Error: File size is larger than the allowed limit.");
+                $errmsg = "File size of {$filesize} is larger than the allowed limit of {$maxsize}";
+                $error  = -6;
+            } else {
+                // Verify MYME type of the file
+                if(in_array($filetype, $allowed)){
+                    // Check whether file exists before uploading it
+                    if(file_exists("upload/" . $filename)) {
+                        echo $filename . " is already present.";
+                        $errmsg = "file exists";
+                        $error  = -1;
+                    } else {
+                        move_uploaded_file($_FILES["uploadfile"]["tmp_name"], "upload/" . $filename);
+                        echo "Your file was uploaded & moved successfully.<br>";
+                        $ufiletype = mime_content_type("upload/" . $filename);
+                        echo "uploaded file type : {$ufiletype}<br>";
+
+                        $errmsg = "The file {$filename} uploaded successfully";
+                        $error  = 0;
+                    } 
+                } else {
+                    echo "Error: There was a problem uploading your file. Please try again.<br><br>";
+                    $ret = in_array($filetype, $allowed);
+                    echo "in_array : " . $ret . "<br><br>";
+                    echo "filetype : " . $filetype . "<br><br>";
+        
+                    $errmsg = "the file type {$filetype} is not allowed";
+                    $error  = -2;
+                }
+            }
         }
     } else {
         echo "Error: " . $_FILES["uploadfile"]["error"];
+
+        $errmsg = "Error: {$_FILES["uploadfile"]["error"]}";
+        $error  = -3;
     }
+} else {
+        $errmsg = "bad request - {$_SERVER["REQUEST_METHOD"]}";
+        $error  = -4;
 }
 ?>
 <script>
-    var err      = {msg: ""};
-    var fileinfo = {file: '{$filename}', type: '{$ufiletype}'};
+    var err      = {msg: '<?php echo $errmsg; ?>', code: <?php echo $error; ?>};
+    var fileinfo = {file: '<?php echo $filename; ?>', type: '<?php echo $ufiletype; ?>', status: err};
     var event = new CustomEvent('upload_complete_evt', {detail: fileinfo});
     window.parent.document.dispatchEvent(event);
 </script>
